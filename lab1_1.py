11757c56baa9b948ceea3315d242df07c6c60ab6
Juan Aragon <juanm.aramas@gmail.com>
2021-10-27 05:01:46 +0100

    lab1_1 + gitignore

---
+++ lab1_1.py
@@ -0,0 +1,24 @@
+from board import D4
+import adafruit_dht
+import time as time
+
+import sys
+
+dht_device = adafruit_dht.DHT11(D4)
+
+
+original_stdout = sys.stdout # Save a reference to the original standard output
+with open('filename.txt', 'w') as f:
+
+    while True:
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+        named_tuple = time.localtime() # get struct_time
+        time_string = time.strftime("%m/%d/%Y %H:%M:%S", named_tuple)
+
+        sys.stdout = f # Change the standard output to the file we created.
+        print(time_string,temperature,humidity)
+    
+        time.sleep(1)
+
+    sys.stdout = original_stdout # Reset the standard output to its original value
